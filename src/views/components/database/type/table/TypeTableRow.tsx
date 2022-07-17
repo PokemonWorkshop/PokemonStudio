64651dcfd8dd833c2dba1729f92761ea9a549793
NuriYuri <youri.54@live.fr>
2022-07-17 13:27:15 +0000

    Improve type table so we're less lost in what we're doing CU-2ju4cn8

--- src/views/components/database/type/table/TypeTableRow.tsx
+++ src/views/components/database/type/table/TypeTableRow.tsx
@@ -3,11 +3,13 @@ import TypeModel from '@modelEntities/type/Type.model';
 
 import { TypeTableRowContainer, TypeIconListContainer } from './TypeTableContainers';
 import { TypeTableCategory, RatioCategoryIcon } from '@components/categories';
+import styled from 'styled-components';
 
 type TypeTableRowProps = {
   currentType: TypeModel;
   allTypes: TypeModel[];
   editType: (type: TypeModel) => void;
+  setHoveredDefensiveType: (value: string) => void;
 };
 
 const getRatio = (currentType: TypeModel, type: TypeModel, allTypes: TypeModel[]) => {
@@ -18,10 +20,19 @@ const getRatio = (currentType: TypeModel, type: TypeModel, allTypes: TypeModel[]
   return 'neutral';
 };
 
-export const TypeTableRow = ({ currentType, allTypes, editType }: TypeTableRowProps) => {
+const TypeCategoryContainer = styled.span`
+  background-color: ${({ theme }) => theme.colors.dark16};
+  position: sticky;
+  left: 0;
+  display: flex;
+`;
+
+export const TypeTableRow = ({ currentType, allTypes, editType, setHoveredDefensiveType }: TypeTableRowProps) => {
   return (
     <TypeTableRowContainer>
-      <TypeTableCategory type={currentType.dbSymbol}>{currentType.name()}</TypeTableCategory>
+      <TypeCategoryContainer className="type-indicator" onMouseEnter={() => setHoveredDefensiveType('__undef__')}>
+        <TypeTableCategory type={currentType.dbSymbol}>{currentType.name()}</TypeTableCategory>
+      </TypeCategoryContainer>
       <TypeIconListContainer>
         {allTypes.map((type) => (
           <RatioCategoryIcon
@@ -30,6 +41,7 @@ export const TypeTableRow = ({ currentType, allTypes, editType }: TypeTableRowPr
             defensiveType={type}
             allTypes={allTypes}
             editType={editType}
+            setHoveredDefensiveType={setHoveredDefensiveType}
             key={`${currentType.name()}-${type.name()}`}
           />
         ))}
