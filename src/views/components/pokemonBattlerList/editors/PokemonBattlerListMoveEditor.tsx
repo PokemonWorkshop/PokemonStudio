3924cb8f1b604ef083b4fcb68da87f3a36a3cd60
Palbolsky <rodolphe0904@wanadoo.fr>
2022-07-22 21:17:00 +0000

    Improve the trainer or wild Pokémon moves selects in editors by adding the possibility to set on "No move"

--- src/views/components/pokemonBattlerList/editors/PokemonBattlerListMoveEditor.tsx
+++ src/views/components/pokemonBattlerList/editors/PokemonBattlerListMoveEditor.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useRefreshUI } from '@components/editor';
 import { InputWithTopLabelContainer, Label, PaddedInputContainer } from '@components/inputs';
 import { InputGroupCollapse } from '@components/inputs/InputContainerCollapse';
 import Encounter, { createExpandPokemonSetup } from '@modelEntities/Encounter';
-import { useTranslation } from 'react-i18next';
-import { SelectMove } from '@components/selects';
+import { TFunction, useTranslation } from 'react-i18next';
+import { SelectDataGeneric } from '@components/selects';
+import { ProjectData } from '@src/GlobalStateProvider';
+import { SelectOption } from '@components/SelectCustom/SelectCustomPropsInterface';
+import { useProjectMoves } from '@utils/useProjectData';
 
 const findOrCreateMoves = (battler: Encounter) => {
   const moves = battler.expandPokemonSetup.find((setup) => setup.type === 'moves');
@@ -16,30 +19,47 @@ const findOrCreateMoves = (battler: Encounter) => {
   return moves;
 };
 
+const getMoveOptions = (allMoves: ProjectData['moves'], t: TFunction<('pokemon_battler_list' | 'database_moves')[]>): SelectOption[] => {
+  const moveOptions = Object.entries(allMoves)
+    .map(([value, moveData]) => ({ value, label: moveData.name(), index: moveData.id }))
+    .sort((a, b) => a.index - b.index)
+    .map((data) => ({ value: data.value, label: data.label }));
+  moveOptions.unshift({ value: '__remove__', label: t('pokemon_battler_list:none') });
+  return moveOptions;
+};
+
+const getData = (move: string, allMoves: ProjectData['moves'], t: TFunction<('pokemon_battler_list' | 'database_moves')[]>): SelectOption => {
+  const name = allMoves[move]?.name();
+  return { value: move, label: name ? name : move === '__remove__' ? t('pokemon_battler_list:none') : t('database_moves:move_deleted') };
+};
+
 type PokemonBattlerListMoveEditorProps = {
   battler: Encounter;
   collapseByDefault: boolean;
 };
 
 export const PokemonBattlerListMoveEditor = ({ battler, collapseByDefault }: PokemonBattlerListMoveEditorProps) => {
-  const { t } = useTranslation('pokemon_battler_list');
+  const { t } = useTranslation(['pokemon_battler_list', 'database_moves']);
   const moves = findOrCreateMoves(battler);
+  const { projectDataValues: movesData } = useProjectMoves();
+  const moveOptions = useMemo(() => getMoveOptions(movesData, t), [movesData, t]);
   const refreshUI = useRefreshUI();
 
   return (
-    <InputGroupCollapse title={t(`moves_title`)} gap="24px" collapseByDefault={collapseByDefault || undefined}>
+    <InputGroupCollapse title={t(`pokemon_battler_list:moves_title`)} gap="24px" collapseByDefault={collapseByDefault || undefined}>
       {moves.type === 'moves' && (
         <PaddedInputContainer size="s">
           {moves.value.map((move, index) => (
             <InputWithTopLabelContainer key={`${move}-${index}`}>
-              <Label htmlFor={`move-${index}`}>{t('move', { id: index + 1 })}</Label>
-              <SelectMove
-                dbSymbol={move}
+              <Label htmlFor={`move-${index}`}>{t('pokemon_battler_list:move', { id: index + 1 })}</Label>
+              <SelectDataGeneric
+                data={getData(move, movesData, t)}
+                options={moveOptions}
                 onChange={(selected) => refreshUI((moves.value[index] = selected.value))}
-                noLabel
+                noOptionsText={t('database_moves:no_option')}
+                error={!movesData[move] && move !== '__undef__' && move !== '__remove__'}
                 noneValue
-                overwriteNoneValue={t('by_default')}
-                noneValueIsError={false}
+                overwriteNoneValue={t('pokemon_battler_list:by_default')}
               />
             </InputWithTopLabelContainer>
           ))}
