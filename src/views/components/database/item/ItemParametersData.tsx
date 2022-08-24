7d1a07fc20a2dc80b41b6cd6dbc49424a9f8154e
Palbolsky <rodolphe0904@wanadoo.fr>
2022-08-24 20:32:22 +0200

    Add an empty state text in the ItemParametersData component

--- src/views/components/database/item/ItemParametersData.tsx
+++ src/views/components/database/item/ItemParametersData.tsx
@@ -5,11 +5,18 @@ import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { DataBlockWithTitle } from '../dataBlocks';
 
-const ParemeterContainer = styled.div`
+const ParameterContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 4px;
 `;
+
+const NoParameterContainer = styled.div`
+  ${({ theme }) => theme.fonts.normalRegular}
+  color: ${({ theme }) => theme.colors.text500};
+`;
+
+const atLeastOneParameter = (item: ItemModel) => item.isBattleUsable || item.isMapUsable || item.isLimited || item.isHoldable;
 
 type ItemParemetersDataProps = {
   item: ItemModel;
@@ -21,14 +28,17 @@ export const ItemParametersData = ({ item, onClick }: ItemParemetersDataProps) =
   const isDisabled = item.lockedEditors.includes('parameters');
   return (
     <DataBlockWithTitle size="fourth" title={t('params')} disabled={isDisabled} onClick={isDisabled ? undefined : onClick}>
-      {!isDisabled && (
-        <ParemeterContainer>
-          {item.isBattleUsable && <Tag>{t('battle')}</Tag>}
-          {item.isMapUsable && <Tag>{t('map')}</Tag>}
-          {item.isLimited && <Tag>{t('limited')}</Tag>}
-          {item.isHoldable && <Tag>{t('holdable')}</Tag>}
-        </ParemeterContainer>
-      )}
+      {!isDisabled &&
+        (atLeastOneParameter(item) ? (
+          <ParameterContainer>
+            {item.isBattleUsable && <Tag>{t('battle')}</Tag>}
+            {item.isMapUsable && <Tag>{t('map')}</Tag>}
+            {item.isLimited && <Tag>{t('limited')}</Tag>}
+            {item.isHoldable && <Tag>{t('holdable')}</Tag>}
+          </ParameterContainer>
+        ) : (
+          <NoParameterContainer>{t('no_parameter')}</NoParameterContainer>
+        ))}
     </DataBlockWithTitle>
   );
 };
