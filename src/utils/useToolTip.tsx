de834c2dfb54b375ffe093a37941fbd0f0dacc7f
NuriYuri <youri.54@live.fr>
2022-11-02 17:20:54 +0000

    CU-2jmbzza Add tooltip to navigation bar

--- src/utils/useToolTip.tsx
+++ src/utils/useToolTip.tsx
@@ -6,6 +6,7 @@ const ToolTipContainer = styled.div`
   visibility: hidden;
   position: absolute;
   z-index: 100000;
+  transition: visibility 0s ease-in 300ms;
 `;
 
 const ToolTipElement = styled.div`
@@ -14,6 +15,18 @@ const ToolTipElement = styled.div`
   border-radius: 4px;
   background-color: ${({ theme }) => theme.colors.dark8};
   color: ${({ theme }) => theme.colors.text100};
+  ${({ theme }) => theme.fonts.normalRegular}
+
+  &.left-arrow::after {
+    content: ' ';
+    position: absolute;
+    top: 50%;
+    right: 100%; /* To the left of the tooltip */
+    margin-top: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent ${({ theme }) => theme.colors.dark8} transparent transparent;
+  }
 `;
 
 type ToolTipStyles = {
@@ -36,6 +49,7 @@ type ToolTipStyles = {
 };
 
 const TOOL_TIP_ELEMENT_SPACING = '4px';
+const TOOL_TIP_ELEMENT_WITH_ARROW_SPACING = '9px';
 
 type ToolTipPosition =
   | 'top-begin'
@@ -51,7 +65,7 @@ type ToolTipPosition =
   | 'right-center'
   | 'right-end';
 
-const computeToolTipStyles = <U extends HTMLElement>(position: ToolTipPosition, e: MouseEvent<U>): ToolTipStyles => {
+const computeToolTipStyles = <U extends HTMLElement>(position: ToolTipPosition, e: MouseEvent<U>, arrow: boolean): ToolTipStyles => {
   const element = e.currentTarget;
   // Get screen accurate coordinate of currentTarget
   const rect = e.currentTarget.getBoundingClientRect();
@@ -68,66 +82,81 @@ const computeToolTipStyles = <U extends HTMLElement>(position: ToolTipPosition, 
     case 'bottom-begin':
       return {
         anchor: { top: bottom, left: left, visibility: 'visible' },
-        element: { marginTop: TOOL_TIP_ELEMENT_SPACING },
+        element: { marginTop: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'bottom-center':
       return {
         anchor: { top: bottom, left: middleX, visibility: 'visible' },
-        element: { left: '-50%', marginTop: TOOL_TIP_ELEMENT_SPACING },
+        element: { left: '-50%', marginTop: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'bottom-end':
       return {
         anchor: { top: bottom, left: right, visibility: 'visible' },
-        element: { left: '-100%', marginTop: TOOL_TIP_ELEMENT_SPACING },
+        element: { left: '-100%', marginTop: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'left-begin':
       return {
         anchor: { top: top, left: left, transform: 'translateX(-100%)', visibility: 'visible' },
-        element: { marginRight: TOOL_TIP_ELEMENT_SPACING },
+        element: { marginRight: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'left-center':
       return {
         anchor: { top: middleY, left: left, transform: 'translateX(-100%)', visibility: 'visible' },
-        element: { transform: 'translateY(-50%)', marginRight: TOOL_TIP_ELEMENT_SPACING },
+        element: { transform: 'translateY(-50%)', marginRight: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'left-end':
       return {
         anchor: { top: bottom, left: left, transform: 'translateX(-100%)', visibility: 'visible' },
-        element: { transform: 'translateY(-100%)', marginRight: TOOL_TIP_ELEMENT_SPACING },
+        element: { transform: 'translateY(-100%)', marginRight: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'right-begin':
       return {
         anchor: { top: top, left: right, visibility: 'visible' },
-        element: { marginLeft: TOOL_TIP_ELEMENT_SPACING },
+        element: { marginLeft: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'right-center':
       return {
         anchor: { top: middleY, left: right, visibility: 'visible' },
-        element: { transform: 'translateY(-50%)', marginLeft: TOOL_TIP_ELEMENT_SPACING },
+        element: { transform: 'translateY(-50%)', marginLeft: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'right-end':
       return {
         anchor: { top: bottom, left: right, visibility: 'visible' },
-        element: { transform: 'translateY(-100%)', marginLeft: TOOL_TIP_ELEMENT_SPACING },
+        element: { transform: 'translateY(-100%)', marginLeft: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'top-begin':
       return {
         anchor: { top: top, left: left, transform: 'translateY(-100%)', visibility: 'visible' },
-        element: { marginBottom: TOOL_TIP_ELEMENT_SPACING },
+        element: { marginBottom: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'top-center':
       return {
         anchor: { top: top, left: middleX, transform: 'translateY(-100%)', visibility: 'visible' },
-        element: { left: '-50%', marginBottom: TOOL_TIP_ELEMENT_SPACING },
+        element: { left: '-50%', marginBottom: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     case 'top-end':
       return {
         anchor: { top: top, left: right, transform: 'translateY(-100%)', visibility: 'visible' },
-        element: { left: '-100%', marginBottom: TOOL_TIP_ELEMENT_SPACING },
+        element: { left: '-100%', marginBottom: arrow ? TOOL_TIP_ELEMENT_WITH_ARROW_SPACING : TOOL_TIP_ELEMENT_SPACING },
       };
     default:
       return ((v: never) => v)(position);
   }
+};
+
+const ARROW_MAP: Record<ToolTipPosition, 'left-arrow' | 'right-arrow' | 'top-arrow' | 'bottom-arrow'> = {
+  'top-begin': 'bottom-arrow',
+  'top-center': 'bottom-arrow',
+  'top-end': 'bottom-arrow',
+  'bottom-begin': 'top-arrow',
+  'bottom-center': 'top-arrow',
+  'bottom-end': 'top-arrow',
+  'left-begin': 'right-arrow',
+  'left-center': 'right-arrow',
+  'left-end': 'right-arrow',
+  'right-begin': 'left-arrow',
+  'right-center': 'left-arrow',
+  'right-end': 'left-arrow',
 };
 
 /**
@@ -143,21 +172,25 @@ const computeToolTipStyles = <U extends HTMLElement>(position: ToolTipPosition, 
  * )
  */
 export const useToolTip = <U extends HTMLElement>() => {
-  const [toolTipText, setToolTipText] = useState('');
+  const [toolTip, setToolTip] = useState<{ text: string; className?: string }>({ text: '', className: undefined });
   const [toolTipStyles, setToolTipStyles] = useState<ToolTipStyles>({ anchor: {}, element: {} });
 
   return {
-    buildOnMouseEnter: (text: string, position: ToolTipPosition) => (e: MouseEvent<U>) => {
-      setToolTipText(text);
-      setToolTipStyles(computeToolTipStyles(position, e));
-    },
+    buildOnMouseEnter:
+      (text: string, position: ToolTipPosition, arrow = false) =>
+      (e: MouseEvent<U>) => {
+        setToolTip({ text, className: arrow ? ARROW_MAP[position] : undefined });
+        setToolTipStyles(computeToolTipStyles(position, e, arrow));
+      },
     onMouseLeave: () => {
       setToolTipStyles((current) => ({ ...current, anchor: { ...current.anchor, visibility: undefined } }));
     },
     renderToolTip: () =>
       ReactDOM.createPortal(
         <ToolTipContainer style={toolTipStyles.anchor}>
-          <ToolTipElement style={toolTipStyles.element}>{toolTipText}</ToolTipElement>
+          <ToolTipElement style={toolTipStyles.element} className={toolTip.className}>
+            {toolTip.text}
+          </ToolTipElement>
         </ToolTipContainer>,
         document.querySelector('#tooltip') || document.createElement('div')
       ),
