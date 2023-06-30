f84c2f35c9e6c8889247c5d7d75c1a980e38e2f4
Zøzo <benbar2412@gmail.com>
2023-06-30 20:43:23 +0000

    Implementation of total statistics

--- src/views/components/database/pokemon/pokemonDataBlock/StatisticsDataBlock.tsx
+++ src/views/components/database/pokemon/pokemonDataBlock/StatisticsDataBlock.tsx
@@ -6,10 +6,13 @@ import { PokemonDataProps } from '../PokemonDataPropsInterface';
 export const StatisticsDataBlock = ({ pokemonWithForm, dialogsRef }: PokemonDataProps) => {
   const { form } = pokemonWithForm;
   const { t } = useTranslation('database_pokemon');
+
+  const totalBaseStats = form.baseHp + form.baseAtk + form.baseDfe + form.baseAts + form.baseDfs + form.baseSpd
+
   return (
     <DataBlockWithTitle size="half" title={t('stats')} onClick={() => dialogsRef.current?.openDialog('stats')}>
       <DataGrid columns="1fr 1fr" gap="64px">
-        <DataFieldgroup title={t('base_stats')}>
+        <DataFieldgroup title={t('base_stats')} data={totalBaseStats}>
           <DataFieldgroupField label={t('hp')} data={form.baseHp} />
           <DataFieldgroupField label={t('attack')} data={form.baseAtk} />
           <DataFieldgroupField label={t('defense')} data={form.baseDfe} />
