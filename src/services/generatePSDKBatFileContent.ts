7b9dcda6314a427cd198748531c99cfac28b2543
NuriYuri <youri.54@live.fr>
2023-07-19 15:56:34 +0000

    Allow Pokémon Studio to start PSDK on MacOS (arm64)

--- src/services/generatePSDKBatFileContent.ts
+++ src/services/generatePSDKBatFileContent.ts
@@ -5,3 +5,10 @@ CHCP 65001
 set PSDK_BINARY_PATH=${getPSDKBinariesPath()}\\
 "%PSDK_BINARY_PATH%ruby.exe" --disable=gems,rubyopt,did_you_mean Game.rb %*
 `;
+
+export const generateGameMacFileContent = (projectPath: string) => `#!/bin/bash
+cd "${getPSDKBinariesPath()}/ruby-dist"
+source ./setup.sh
+cd "${projectPath}"
+PSDK_BINARY_PATH="${getPSDKBinariesPath()}/" ruby Game.rb "$@"
+`;
