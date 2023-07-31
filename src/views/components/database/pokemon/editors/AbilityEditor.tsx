9cb27b73cfebbe5e3896da5bcf0f0cf0a1e25244
Menta <poree47@gmail.com>
2023-07-31 18:04:34 +0000

    refactor(AbitilyEditor): generalize the component and avoid duplicate

--- src/views/components/database/pokemon/editors/AbilityEditor.tsx
+++ src/views/components/database/pokemon/editors/AbilityEditor.tsx
@@ -1,26 +1,28 @@
-import { Editor, useRefreshUI } from '@components/editor';
+import { Editor } from '@components/editor';
 import { EditorHandlingClose, useEditorHandlingClose } from '@components/editor/useHandleCloseEditor';
 import { InputContainer, InputWithTopLabelContainer, Label } from '@components/inputs';
 import { SelectAbility } from '@components/selects';
-import { StudioCreature } from '@modelEntities/creature';
 import { DbSymbol } from '@modelEntities/dbSymbol';
 import { useCreaturePage } from '@utils/usePage';
-import React, { forwardRef, FunctionComponent, useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useUpdateForm } from './useUpdateForm';
+
+const ABILITY_TEXT_KEYS = ['ability_1', 'ability_2', 'hidden_ability'] as const;
 
 export const AbilityEditor = forwardRef<EditorHandlingClose>((_, ref) => {
   const { t } = useTranslation('database_pokemon');
   const { creature, form } = useCreaturePage();
   const updateForm = useUpdateForm(creature, form);
-  // TODO: geneneralize this in the future
-  const [ab1, setAb1] = useState(form.abilities[0] || ('__undef__' as DbSymbol));
-  const [ab2, setAb2] = useState(form.abilities[1] || ('__undef__' as DbSymbol));
-  const [ab3, setAb3] = useState(form.abilities[2] || ('__undef__' as DbSymbol));
+  const [abilities, setAbilities] = useState([
+    form.abilities[0] || ('__undef__' as DbSymbol),
+    form.abilities[1] || ('__undef__' as DbSymbol),
+    form.abilities[2] || ('__undef__' as DbSymbol),
+  ]);
 
   const onClose = () => {
     updateForm({
-      abilities: [ab1, ab2, ab3],
+      abilities: [...abilities],
     });
   };
   useEditorHandlingClose(ref, onClose);
@@ -28,18 +30,22 @@ export const AbilityEditor = forwardRef<EditorHandlingClose>((_, ref) => {
   return (
     <Editor type="edit" title={t('abilities')}>
       <InputContainer>
-        <InputWithTopLabelContainer>
-          <Label htmlFor="ability_1">{t('ability_1')}</Label>
-          <SelectAbility dbSymbol={ab1} onChange={(newDbSymbol) => setAb1(newDbSymbol as DbSymbol)} noLabel />
-        </InputWithTopLabelContainer>
-        <InputWithTopLabelContainer>
-          <Label htmlFor="ability_2">{t('ability_2')}</Label>
-          <SelectAbility dbSymbol={ab2} onChange={(newDbSymbol) => setAb2(newDbSymbol as DbSymbol)} noLabel />
-        </InputWithTopLabelContainer>
-        <InputWithTopLabelContainer>
-          <Label htmlFor="hidden_ability">{t('hidden_ability')}</Label>
-          <SelectAbility dbSymbol={ab3} onChange={(newDbSymbol) => setAb3(newDbSymbol as DbSymbol)} noLabel />
-        </InputWithTopLabelContainer>
+        {abilities.map((ability, index: number) => {
+          const label = ABILITY_TEXT_KEYS[index];
+          return (
+            <InputWithTopLabelContainer key={index}>
+              <Label htmlFor={label}>{t(label)}</Label>
+              <SelectAbility
+                dbSymbol={ability}
+                onChange={(newDbSymbol) => {
+                  abilities.splice(index, 1, newDbSymbol as DbSymbol);
+                  setAbilities([...abilities]);
+                }}
+                noLabel
+              />
+            </InputWithTopLabelContainer>
+          );
+        })}
       </InputContainer>
     </Editor>
   );
