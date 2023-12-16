69fde8765baffe0312dd4d7c1f2783394e964ead
Palbolsky <palbolsky@gmail.com>
2023-12-16 14:32:04 +0100

    Update style and texts

--- src/views/components/world/map/MapRMXP2StudioUpdate.tsx
+++ src/views/components/world/map/MapRMXP2StudioUpdate.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { BaseButtonStyle, WarningButton } from '@components/buttons';
+import { SecondaryButton } from '@components/buttons';
 import { DataBlockContainer } from '@components/database/dataBlocks';
 import { useRMXP2StudioMapsUpdate } from '@utils/useRMXP2StudioMapsUpdate';
 import { showNotification } from '@utils/showNotification';
 import { useLoaderRef } from '@utils/loaderContext';
 import { MapUpdateContainer } from './MapUpdate';
 
-const MapRMXPUpdateContainer = styled(MapUpdateContainer)`
-  ${BaseButtonStyle} {
-    white-space: nowrap;
-    min-width: 180px;
-  }
-`;
+const MapRMXPUpdateContainer = MapUpdateContainer;
 
 export const MapRMXP2StudioUpdate = () => {
   const { t } = useTranslation('database_maps');
@@ -33,13 +27,13 @@ export const MapRMXP2StudioUpdate = () => {
   };
 
   return (
-    <DataBlockContainer size="full" color="warning" data-disabled="true">
+    <DataBlockContainer size="full" color="light" data-disabled="true">
       <MapRMXPUpdateContainer>
         <div>
           <h2>{t('update_rmxp_maps')}</h2>
           <span className="message">{t('update_rmxp_maps_message')}</span>
         </div>
-        <WarningButton onClick={handleUpdate}>{t('update_rmxp_maps_button')}</WarningButton>
+        <SecondaryButton onClick={handleUpdate}>{t('update_rmxp_maps_button')}</SecondaryButton>
       </MapRMXPUpdateContainer>
     </DataBlockContainer>
   );
