18523d24f372932d33f1931bcaf21a77d2f48b95
Zozo <benbar2412@gmail.com>
2023-12-23 20:31:58 +0100

    Converting Showdown to Studio (not finished)

--- src/utils/showdownUtils.tsx
+++ src/utils/showdownUtils.tsx
@@ -1,26 +1,84 @@
+import { StudioExpandPokemonSetup, StudioGroupEncounter, StudioIvEv } from '@modelEntities/groupEncounter';
 import { Teams, PokemonSet } from '@pkmn/sim';
+import { DbSymbol } from '@modelEntities/dbSymbol';
+import { StatsTable } from '@pkmn/types';
 
-export const cleanShowdownData = (data: string): string => {
-  return data
-    .split('\n')
-    .filter((line) => line.trim() !== '')
-    .join('\n');
-}; // Si jamais il y a des retour à la ligne vide
-
-export const handleShowdownInputChange = (data: string) => {
-  const cleanedData = cleanShowdownData(data);
-  console.log(cleanedData);
-
-  const team = Teams.import(cleanedData);
-  console.log(team);
-
-  if (!team) {
-    console.error('Erreur lors de l’importation de l’équipe');
-    return;
+const isTeamValid = (pokemonSets: PokemonSet[]): boolean => {
+  for (const set of pokemonSets) {
+    if (!set.species || !set.ability) {
+      return false;
+    }
   }
-
-  const convertedTeam = team.map(convertShowdownSetToStudioFormat);
-  // console.log(convertedTeam);
+  return true;
 };
 
-const convertShowdownSetToStudioFormat = () => {};
+const splitIntoTeams = (data: string): string[] => {
+  return data
+    .trim()
+    .split('\n\n')
+    .filter((team) => team.trim() !== '');
+};
+
+export const convertShowdownInputChange = (data: string) => {
+  const teamsData = splitIntoTeams(data);
+
+  const convertedTeams = teamsData.flatMap((teamData) => {
+    const team = Teams.import(teamData);
+
+    if (!team || !isTeamValid(team)) {
+      return [];
+    }
+
+    return team.map(convertShowdownToStudioFormat);
+  });
+
+  if (teamsData.length === convertedTeams.length) return [];
+
+  console.log(convertedTeams.length);
+  console.log(teamsData.length);
+
+  return convertedTeams;
+};
+
+const convertShowdownToStudioFormat = (set: PokemonSet): StudioGroupEncounter => {
+  const encounter: StudioGroupEncounter = {
+    specie: set.species.toLowerCase() as DbSymbol,
+    form: 0,
+    shinySetup: set.shiny ? { kind: 'rate', rate: 1 } : { kind: 'automatic', rate: -1 },
+    levelSetup: { kind: 'fixed', level: set.level ?? 100 },
+    randomEncounterChance: 1,
+    expandPokemonSetup: buildExpandPokemonSetupFromShowdown(set),
+  };
+
+  return encounter;
+};
+
+const buildExpandPokemonSetupFromShowdown = (set: PokemonSet) => {
+  const setupFields: StudioExpandPokemonSetup[] = [
+    { type: 'originalTrainerName', value: '' },
+    { type: 'ability', value: set.ability?.toLowerCase().replace(' ', '_') as DbSymbol },
+    { type: 'caughtWith', value: 'poke_ball' as DbSymbol },
+    { type: 'evs', value: formatStats(set.evs) },
+    { type: 'ivs', value: formatStats(set.ivs) },
+    { type: 'gender', value: set.gender == 'F' ? 2 : set.gender == 'M' ? 1 : -1 },
+    { type: 'itemHeld', value: set.item?.toLowerCase().replace(' ', '_') as DbSymbol },
+    { type: 'loyalty', value: 70 },
+    {
+      type: 'moves',
+      value: set.moves?.slice(0, 4).map((move) => convertMove(move) as DbSymbol),
+    },
+    { type: 'nature', value: set.nature?.toLowerCase() as DbSymbol },
+    { type: 'originalTrainerId', value: 0 },
+    { type: 'rareness', value: -1 },
+  ];
+
+  return setupFields;
+};
+
+const formatStats = (stats: StatsTable): StudioIvEv => {
+  return { hp: stats?.hp ?? 31, atk: stats?.atk ?? 31, dfe: stats?.def ?? 31, spd: stats?.spd ?? 31, ats: stats?.spa ?? 31, dfs: stats?.spe ?? 31 };
+};
+
+const convertMove = (move: string) => {
+  return move.startsWith('Hidden Power') ? 'hidden_power' : move.toLowerCase().replace(/[\s-]+/g, '_');
+};
