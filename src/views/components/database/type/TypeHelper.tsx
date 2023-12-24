f0c32435340dc82fe9a1bdf81ff9c23b0bc620d0
Nuri Yuri <nuriyuri@pokemonworkshop.com>
2023-12-24 15:40:01 +0100

    Fix rendering of type chart helper text

--- src/views/components/database/type/TypeHelper.tsx
+++ src/views/components/database/type/TypeHelper.tsx
@@ -4,10 +4,11 @@ import { TFunction, useTranslation } from 'react-i18next';
 import { assertUnreachable } from '@utils/assertUnreachable';
 import { useGetEntityNameTextUsingTextId } from '@utils/ReadingProjectText';
 import { getEfficiency, StudioType } from '@modelEntities/type';
+import { DbSymbol } from '@modelEntities/dbSymbol';
 
 export type HelperSelectedType = {
-  offensiveType: StudioType | undefined;
-  defensiveType: StudioType | undefined;
+  offensiveType: DbSymbol | undefined;
+  defensiveType: DbSymbol | undefined;
 };
 
 const TypeHelperContainer = styled.span`
@@ -18,22 +19,23 @@ const TypeHelperContainer = styled.span`
 `;
 
 const getHelperText = (
-  typeHelperSelected: HelperSelectedType,
+  offensive: DbSymbol,
+  defensive: DbSymbol,
   allTypes: StudioType[],
   t: TFunction<'database_types'>,
   getTypeName: ReturnType<typeof useGetEntityNameTextUsingTextId>
 ) => {
-  if (!typeHelperSelected.offensiveType || !typeHelperSelected.defensiveType) return '';
-
-  const efficiency = getEfficiency(typeHelperSelected.offensiveType, typeHelperSelected.defensiveType, allTypes);
+  const offensiveType = allTypes.find(({ dbSymbol }) => dbSymbol === offensive) || allTypes[0];
+  const defensiveType = allTypes.find(({ dbSymbol }) => dbSymbol === defensive) || allTypes[0];
+  const efficiency = getEfficiency(offensiveType, defensiveType, allTypes);
   switch (efficiency) {
     case 'high_efficience':
     case 'low_efficience':
     case 'neutral':
     case 'zero_efficience':
       return t(`${efficiency}_helper`, {
-        offensiveType: getTypeName(typeHelperSelected.offensiveType),
-        defensiveType: getTypeName(typeHelperSelected.defensiveType),
+        offensiveType: getTypeName(offensiveType),
+        defensiveType: getTypeName(defensiveType),
       });
     default:
       assertUnreachable(efficiency);
@@ -53,6 +55,8 @@ export const TypeHelper = ({ typeHelperSelected, allTypes }: TypeHelperProps) =>
   return !typeHelperSelected.offensiveType || !typeHelperSelected.defensiveType ? (
     <></>
   ) : (
-    <TypeHelperContainer>{getHelperText(typeHelperSelected, allTypes, t, getTypeName)}</TypeHelperContainer>
+    <TypeHelperContainer>
+      {getHelperText(typeHelperSelected.offensiveType, typeHelperSelected.defensiveType, allTypes, t, getTypeName)}
+    </TypeHelperContainer>
   );
 };
