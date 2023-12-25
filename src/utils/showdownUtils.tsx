617b61b75a91068fec633843208ba1405288a7c1
Zozo <benbar2412@gmail.com>
2023-12-25 18:17:30 +0100

    Finishing touches around showdownUtils

--- src/utils/showdownUtils.tsx
+++ src/utils/showdownUtils.tsx
@@ -3,15 +3,6 @@ import { Teams, PokemonSet } from '@pkmn/sim';
 import { DbSymbol } from '@modelEntities/dbSymbol';
 import { StatsTable } from '@pkmn/types';
 
-const isTeamValid = (pokemonSets: PokemonSet[]): boolean => {
-  for (const set of pokemonSets) {
-    if (!set.species || !set.ability) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const splitIntoTeams = (data: string): string[] => {
   return data
     .trim()
@@ -19,33 +10,37 @@ const splitIntoTeams = (data: string): string[] => {
     .filter((team) => team.trim() !== '');
 };
 
+/**
+ * The Showdown format minimum
+ */
+const isTeamValid = (pokemonSets: PokemonSet[]): boolean => {
+  return pokemonSets.every((set) => set.species && set.ability);
+};
+
 export const convertShowdownInputChange = (data: string) => {
   const teamsData = splitIntoTeams(data);
 
   const convertedTeams = teamsData.flatMap((teamData) => {
-    const team = Teams.import(teamData);
+    const validTeams = Teams.import(teamData);
 
-    if (!team || !isTeamValid(team)) {
+    if (!validTeams || !isTeamValid(validTeams)) {
       return [];
     }
 
-    return team.map(convertShowdownToStudioFormat);
+    return validTeams.map(convertShowdownToStudioFormat);
   });
 
-  if (teamsData.length === convertedTeams.length) return [];
-
-  console.log(convertedTeams.length);
-  console.log(teamsData.length);
+  if (teamsData.length !== convertedTeams.length) return [];
 
   return convertedTeams;
 };
 
 const convertShowdownToStudioFormat = (set: PokemonSet): StudioGroupEncounter => {
   const encounter: StudioGroupEncounter = {
-    specie: set.species.toLowerCase() as DbSymbol,
+    specie: convertToDbSymbol(set.species),
     form: 0,
     shinySetup: set.shiny ? { kind: 'rate', rate: 1 } : { kind: 'automatic', rate: -1 },
-    levelSetup: { kind: 'fixed', level: set.level ?? 100 },
+    levelSetup: { kind: 'fixed', level: set.level ?? 1 },
     randomEncounterChance: 1,
     expandPokemonSetup: buildExpandPokemonSetupFromShowdown(set),
   };
@@ -55,19 +50,15 @@ const convertShowdownToStudioFormat = (set: PokemonSet): StudioGroupEncounter =>
 
 const buildExpandPokemonSetupFromShowdown = (set: PokemonSet) => {
   const setupFields: StudioExpandPokemonSetup[] = [
-    { type: 'originalTrainerName', value: '' },
-    { type: 'ability', value: set.ability?.toLowerCase().replace(' ', '_') as DbSymbol },
-    { type: 'caughtWith', value: 'poke_ball' as DbSymbol },
-    { type: 'evs', value: formatStats(set.evs) },
-    { type: 'ivs', value: formatStats(set.ivs) },
-    { type: 'gender', value: set.gender == 'F' ? 2 : set.gender == 'M' ? 1 : -1 },
-    { type: 'itemHeld', value: set.item?.toLowerCase().replace(' ', '_') as DbSymbol },
-    { type: 'loyalty', value: 70 },
-    {
-      type: 'moves',
-      value: set.moves?.slice(0, 4).map((move) => convertMove(move) as DbSymbol),
-    },
-    { type: 'nature', value: set.nature?.toLowerCase() as DbSymbol },
+    { type: 'ability', value: convertToDbSymbol(set.ability) },
+    { type: 'caughtWith', value: convertToDbSymbol(set.pokeball) || ('poke_ball' as DbSymbol) },
+    { type: 'evs', value: formatStats(set.evs, 0) },
+    { type: 'ivs', value: formatStats(set.ivs, 31) },
+    { type: 'gender', value: convertGender(set.gender) },
+    { type: 'itemHeld', value: convertToDbSymbol(set.item) || ('__undef__' as DbSymbol) },
+    { type: 'loyalty', value: set.happiness ?? 70 },
+    { type: 'moves', value: convertMoves(set.moves) },
+    { type: 'nature', value: convertToDbSymbol(set.nature) },
     { type: 'originalTrainerId', value: 0 },
     { type: 'rareness', value: -1 },
   ];
@@ -75,10 +66,41 @@ const buildExpandPokemonSetupFromShowdown = (set: PokemonSet) => {
   return setupFields;
 };
 
-const formatStats = (stats: StatsTable): StudioIvEv => {
-  return { hp: stats?.hp ?? 31, atk: stats?.atk ?? 31, dfe: stats?.def ?? 31, spd: stats?.spd ?? 31, ats: stats?.spa ?? 31, dfs: stats?.spe ?? 31 };
+const convertToDbSymbol = (str: string | undefined): DbSymbol => str?.toLowerCase().replace(/[\s-]+/g, '_') as DbSymbol;
+
+const convertGender = (gender: string): 0 | 1 | 2 | -1 => {
+  switch (gender) {
+    case 'F':
+      return 2;
+    case 'M':
+      return 1;
+    default:
+      return -1;
+  }
+};
+
+const formatStats = (stats: StatsTable, defaultValue: number): StudioIvEv => {
+  return {
+    hp: stats?.hp ?? defaultValue,
+    atk: stats?.atk ?? defaultValue,
+    dfe: stats?.def ?? defaultValue,
+    spd: stats?.spd ?? defaultValue,
+    ats: stats?.spa ?? defaultValue,
+    dfs: stats?.spe ?? defaultValue,
+  };
+};
+
+const convertMoves = (moves: string[] | undefined) => {
+  const defaultMove = '__undef__' as DbSymbol;
+  const movesConverted = moves ? moves.slice(0, 4).map((move) => convertMove(move)) : [];
+
+  while (movesConverted.length < 4) {
+    movesConverted.push(defaultMove);
+  }
+
+  return movesConverted;
 };
 
 const convertMove = (move: string) => {
-  return move.startsWith('Hidden Power') ? 'hidden_power' : move.toLowerCase().replace(/[\s-]+/g, '_');
+  return move.startsWith('Hidden Power') ? ('hidden_power' as DbSymbol) : convertToDbSymbol(move);
 };
