96e6a41d331ab0c42294cfb0be0abacaae720547
Palbolsky <palbolsky@gmail.com>
2024-01-17 22:00:24 +0100

    Remove axios

--- src/backendTasks/downloadFile.ts
+++ src/backendTasks/downloadFile.ts
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { IpcMainEvent } from 'electron';
+import { IpcMainEvent, net } from 'electron';
 import log from 'electron-log';
 import fs from 'fs';
 import path from 'path';
@@ -25,23 +24,44 @@ const getDestPath = (destFile: DestFile) => {
   }
 };
 
+const getSpeed = (speed: { lastTime: number; speed: number; lastBytesWritten: number }, currentBytesWritten: number) => {
+  const currentTime = Date.now();
+  if (currentTime >= speed.lastTime + 1000) {
+    return { lastTime: currentTime, speed: currentBytesWritten - speed.lastBytesWritten, lastBytesWritten: currentBytesWritten };
+  }
+  return speed;
+};
+
 const downloadFile = async (payload: DownloadFileInput, event: IpcMainEvent, channels: ChannelNames) => {
   const destPath = getDestPath(payload.dest);
   if (fs.existsSync(destPath)) {
     fs.unlinkSync(destPath);
   }
   const writer = fs.createWriteStream(getDestPath(payload.dest));
-  await axios({
-    method: 'get',
-    url: payload.url,
-    responseType: 'stream',
-    onDownloadProgress: ({ rate, progress }) => {
-      sendProgress(event, channels, { step: (progress ?? 0) * 100, total: 0, stepText: (rate || 0).toString() });
-    },
-  }).then(async (response) => {
-    response.data.pipe(writer);
-    return await finished(writer);
+  const request = net.request(payload.url);
+
+  request.on('response', (response) => {
+    const length = Number(response.headers['content-length']);
+    let speed = { lastTime: 0, speed: 0, lastBytesWritten: 0 };
+
+    response.on('data', (chunk) => {
+      writer.write(chunk);
+      const progress = writer.bytesWritten / length;
+      speed = getSpeed(speed, writer.bytesWritten);
+      sendProgress(event, channels, { step: (progress ?? 0) * 100, total: 0, stepText: speed.speed.toString() });
+    });
+    response.on('end', () => {
+      sendProgress(event, channels, { step: 100, total: 0, stepText: speed.speed.toString() });
+      writer.close();
+    });
   });
+  request.on('error', (error) => {
+    throw error;
+  });
+  request.end();
+
+  await finished(writer);
+
   if (!payload.sha1) return;
 
   const currentSha1 = await calculateFileSha1(destPath);
--- src/backendTasks/requestFile.ts
+++ src/backendTasks/requestFile.ts
@@ -1,13 +1,29 @@
+import { net } from 'electron';
 import log from 'electron-log';
 import { defineBackendServiceFunction } from './defineBackendServiceFunction';
-import axios from 'axios';
 
 export type RequestFileInput = { url: string };
 export type RequestFileOutput = { file: unknown };
 
 export const requestFile = async (url: string) => {
-  const response = await axios({ method: 'get', url, responseType: 'json' });
-  return response.data;
+  return new Promise((resolve, reject) => {
+    const request = net.request(url);
+    let data = '';
+    request.on('response', (response) => {
+      response.on('end', async () => {
+        try {
+          resolve(JSON.parse(data.trim()));
+        } catch (error) {
+          reject(error);
+        }
+      });
+      response.on('data', (chunk) => {
+        data = data + chunk.toString('utf-8');
+      });
+    });
+    request.on('error', (error) => reject(error));
+    request.end();
+  });
 };
 
 export const requestFileService = async (payload: RequestFileInput) => {
