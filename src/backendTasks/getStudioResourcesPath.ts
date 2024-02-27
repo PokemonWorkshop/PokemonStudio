89c88017ab7c1ed33514aeb8cc39329a25b9d8fe
Palbolsky <palbolsky@gmail.com>
2024-02-27 17:51:18 +0100

    Fix typo

--- src/backendTasks/getStudioResourcesPath.ts
+++ src/backendTasks/getStudioResourcesPath.ts
@@ -4,8 +4,8 @@ import fs from 'fs';
 
 export const getStudioResourcesPath = () => {
   if (process.env.NODE_ENV === 'development') return getAppRootPath();
-  if (process.env.APPDATA) return createFolder(path.join(process.env.APPDATA, './pokemon-studio-resources'));
-  if (process.env.HOME) return createFolder(path.join(process.env.HOME, './pokemon-studio-resources'));
+  if (process.env.APPDATA) return createFolder(path.join(process.env.APPDATA, '.pokemon-studio-resources'));
+  if (process.env.HOME) return createFolder(path.join(process.env.HOME, '.pokemon-studio-resources'));
 
   return getAppRootPath();
 };
