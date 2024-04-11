6cac8c4e3c0597890728af0d248f40d38a2df5ca
NuriYuri <youri.54@live.fr>
2024-04-11 23:20:44 +0200

    Fix #277 Open the logs properly (#278)
    Co-authored-by: NuriYuri <yuri@pokemonworkshop.com>

--- src/backendTasks/openStudioLogsFolder.ts
+++ src/backendTasks/openStudioLogsFolder.ts
@@ -1,22 +1,27 @@
 import log from 'electron-log';
 import { app, shell } from 'electron';
 import { defineBackendServiceFunction } from './defineBackendServiceFunction';
+import path from 'path';
 
 const getLogsPath = () => {
-  if (process.platform === 'win32') {
-    return `${app.getPath('userData')}\\logs\\renderer.log`;
-  } else if (process.platform === 'linux') {
-    return `~/.config/${app.name}/logs/renderer.log`;
-  } else {
-    return `~/Library/Logs/${app.name}/renderer.log`;
+  try {
+    // Proper way to get them
+    return path.join(app.getPath('logs'), 'renderer.log');
+  } catch (error) {
+    log.error('Failed to open logs the proper way', error);
+    if (process.platform === 'win32') {
+      return `${app.getPath('userData')}\\logs\\renderer.log`;
+    } else if (process.platform === 'linux') {
+      return `~/.config/${app.name}/logs/renderer.log`;
+    } else {
+      return `${process.env.HOME}/Library/Logs/${app.name}/renderer.log`;
+    }
   }
 };
 
 const openStudioLogsFolder = async () => {
-  log.info('open-studio-logs-folder');
-
+  log.info('open-studio-logs-folder', 'called');
   shell.showItemInFolder(getLogsPath());
-  log.info('open-studio-logs-folder/success');
   return {};
 };
 
