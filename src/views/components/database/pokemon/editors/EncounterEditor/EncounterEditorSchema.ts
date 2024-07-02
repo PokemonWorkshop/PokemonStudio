2f20abc9fcea30a5e96b779d42e1103d4385e1d0
Palbolsky <palbolsky@gmail.com>
2024-07-02 12:56:52 +0200

    Fix editor cannot be closed when the genderless toggle is checked

--- src/views/components/database/pokemon/editors/EncounterEditor/EncounterEditorSchema.ts
+++ src/views/components/database/pokemon/editors/EncounterEditor/EncounterEditorSchema.ts
@@ -1,3 +1,6 @@
 import { CREATURE_FORM_VALIDATOR } from '@modelEntities/creature';
+import { z } from 'zod';
 
-export const ENCOUNTER_EDITOR_SCHEMA = CREATURE_FORM_VALIDATOR.pick({ catchRate: true, femaleRate: true, itemHeld: true });
+export const ENCOUNTER_EDITOR_SCHEMA = CREATURE_FORM_VALIDATOR.pick({ catchRate: true, femaleRate: true, itemHeld: true }).merge(
+  z.object({ isGenderLess: z.boolean() })
+);
