3ebda3b6f73e18a2123d4b9b2870ea6a25e7e40f
Karbbone <clement.maillet56@gmail.com>
2024-12-15 12:52:03 +0100

    fix: isIconShown with state

--- src/views/components/inputs/ClearInput.tsx
+++ src/views/components/inputs/ClearInput.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, forwardRef } from 'react';
+import React, { InputHTMLAttributes, forwardRef, useState } from 'react';
 import styled from 'styled-components';
 import { Input } from '.';
 import { ReactComponent as ClearIcon } from '@assets/icons/global/clear-tag-icon.svg';
@@ -41,10 +41,19 @@ type ClearInputProps = InputHTMLAttributes<HTMLInputElement> & {
 
 export const ClearInput = forwardRef<HTMLInputElement, ClearInputProps>((props, ref) => {
   const { onClear, ...inputProps } = props;
+  const [isIconShown, setIsIconShown] = useState(props.value !== '');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (props.onChange) {
+      props.onChange(event);
+    }
+    setIsIconShown(event.target.value !== '');
+  };
+
   return (
     <ClearInputContainer>
-      <Input {...inputProps} ref={ref} />
-      {props.value !== '' && <ClearIcon onClick={onClear} />}
+      <Input {...inputProps} ref={ref} onChange={onChange} />
+      {isIconShown && <ClearIcon onClick={onClear} />}
     </ClearInputContainer>
   );
 });
