3780d816cd38e96a8c253c22eb0985609bf2f20d
Palbolsky <palbolsky@gmail.com>
2025-01-13 16:20:25 +0100

    Cleaning

--- src/views/pages/database/Quest.page.tsx
+++ src/views/pages/database/Quest.page.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
 import { DataBlockWithAction, DataBlockWrapper } from '@components/database/dataBlocks';
 import { DeleteButtonWithIcon } from '@components/buttons';
-
 import { DatabasePageStyle } from '@components/database/DatabasePageStyle';
 import { PageContainerStyle, PageDataConstrainerStyle } from './PageContainerStyle';
-
 import { QuestControlBar, QuestEarnings, QuestFrame, QuestGoals } from '@components/database/quest';
 import { useQuestPage } from '@src/hooks/usePage';
 import { useDialogsRef } from '@src/hooks/useDialogsRef';
 import { QuestEditorAndDeletionKeys, QuestEditorOverlay } from '@components/database/quest/editors/QuestEditorOverlay';
+import React, { useState } from 'react';
 
 export const QuestPage = () => {
   const dialogsRef = useDialogsRef<QuestEditorAndDeletionKeys>();
@@ -18,32 +15,6 @@ export const QuestPage = () => {
   const { t } = useTranslation('database_quests');
   const [goalIndex, setGoalIndex] = useState(0);
   const [earningIndex, setEarningIndex] = useState(0);
-
-  /*const onCloseEditor = () => {
-    const currentObjective = currentEditedQuest.objectives[currentObjectiveIndex];
-    if (
-      currentEditor === 'editGoal' &&
-      (currentObjective.objectiveMethodName === 'objective_speak_to' || currentObjective.objectiveMethodName === 'objective_beat_npc') &&
-      currentObjective.objectiveMethodArgs[1] === ''
-    )
-      return;
-    if (currentEditor === 'editGoal' || currentEditor === 'editEarning') cleaningQuestNaNValues(currentEditedQuest);
-    updateIndexSpeakToBeatNpc(currentEditedQuest);
-    setQuest({ [quest.dbSymbol]: currentEditedQuest });
-    setCurrentEditor(undefined);
-    closeTranslationEditor();
-  };*/
-
-  /*const editors = {
-    new: <QuestNewEditor onClose={() => setCurrentEditor(undefined)} />,
-    frame: <QuestFrameEditor quest={currentEditedQuest} openTranslationEditor={openTranslationEditor} />,
-    importGoal: <QuestGoalImportEditor quest={currentEditedQuest} onClose={() => setCurrentEditor(undefined)} />,
-    editGoal: <QuestGoalEditor quest={currentEditedQuest} objectiveIndex={currentObjectiveIndex} />,
-    newGoal: <QuestNewGoalEditor quest={currentEditedQuest} onClose={() => setCurrentEditor(undefined)} />,
-    importEarning: <QuestEarningImportEditor quest={currentEditedQuest} onClose={() => setCurrentEditor(undefined)} />,
-    editEarning: <QuestEarningEditor quest={currentEditedQuest} earningIndex={currentEarningIndex} />,
-    newEarning: <QuestNewEarningEditor quest={currentEditedQuest} onClose={() => setCurrentEditor(undefined)} />,
-  };*/
 
   return (
     <DatabasePageStyle>
