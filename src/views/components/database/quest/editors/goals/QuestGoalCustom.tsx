7823bfd75fc10507f63c35aeb6ca6ae208608a1c
ralandel <bbruyere@outlook.fr>
2025-01-25 03:20:16 +0100

    custom_objective input

--- src/views/components/database/quest/editors/goals/QuestGoalCustom.tsx
+++ src/views/components/database/quest/editors/goals/QuestGoalCustom.tsx
@@ -1,23 +1,11 @@
-import { Input, InputWithTopLabelContainer, Label, PaddedInputContainer } from '@components/inputs';
-import React, { useRef } from 'react';
+import { InputWithTopLabelContainer, Label, PaddedInputContainer } from '@components/inputs';
 import { useTranslation } from 'react-i18next';
 import { QuestGoalProps } from './QuestGoalProps';
-import { useProjectStudio } from '@hooks/useProjectStudio';
-import { useSetProjectText } from '@utils/ReadingProjectText';
+import { InputNumber2 } from './InputNumber';
+import React from 'react';
 
 export const QuestGoalCustom = ({ objective, refs, checkIsValid }: QuestGoalProps) => {
   const { t } = useTranslation('database_quests');
-  const { defaultFileId } = useProjectStudio().projectStudioValues;
-  const inputRef = useRef<HTMLInputElement>(null);
-  const setText = useSetProjectText();
-
-  const handleChange = () => {
-    if (inputRef.current) {
-      const objectiveArg = typeof objective.objectiveMethodArgs[0] === 'number' ? objective.objectiveMethodArgs[0] : 0;
-      setText(defaultFileId, objectiveArg, inputRef.current.value);
-      if (checkIsValid) checkIsValid();
-    }
-  };
 
   return (
     <PaddedInputContainer>
@@ -25,13 +13,11 @@ export const QuestGoalCustom = ({ objective, refs, checkIsValid }: QuestGoalProp
         <Label htmlFor="custom-objective" required>
           {t('objective_custom')}
         </Label>
-        <Input
+        <InputNumber2
           ref={refs.nameRef}
-          type="text"
           name="custom-objective"
-          defaultValue={objective.objectiveMethodArgs[0] as string}
-          onChange={handleChange}
-          placeholder={t('example_custom_objective')}
+          defaultValue={objective.objectiveMethodArgs[1] as number}
+          onChange={() => checkIsValid && checkIsValid()}
         />
       </InputWithTopLabelContainer>
     </PaddedInputContainer>
