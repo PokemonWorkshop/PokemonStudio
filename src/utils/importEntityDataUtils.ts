00a8c4fc36d9fef8ed8a3caf7c8bb04823a7a421
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-02-03 14:55:44 +0100

    Add cloneEntity

--- src/utils/importEntityDataUtils.ts
+++ src/utils/importEntityDataUtils.ts
@@ -1,11 +1,14 @@
 import { StudioTrainer } from '@modelEntities/trainer';
+import { cloneEntity } from './cloneEntity';
 
 export const importTrainerData = (trainer: StudioTrainer, dataToImport: StudioTrainer): StudioTrainer => {
+  const cloneData = cloneEntity(dataToImport);
+
   return {
     ...trainer,
-    bagEntries: dataToImport.bagEntries,
-    party: dataToImport.party,
-    additionalDialogs: dataToImport.additionalDialogs,
-    resources: dataToImport.resources,
+    bagEntries: cloneData.bagEntries,
+    party: cloneData.party,
+    additionalDialogs: cloneData.additionalDialogs,
+    resources: cloneData.resources,
   };
 };
