88bc27dc78d39472f3a214e74aab54b0ae083fdc
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-06 23:59:38 +0100

    New moves can be created by importing data from another move

--- src/utils/importEntityDataUtils.ts
+++ src/utils/importEntityDataUtils.ts
@@ -1,4 +1,5 @@
 import { StudioTrainer } from '@modelEntities/trainer';
+import { StudioMove } from '@modelEntities/move';
 import { cloneEntity } from './cloneEntity';
 
 export const importTrainerData = (trainer: StudioTrainer, dataToImport: StudioTrainer): StudioTrainer => {
@@ -10,5 +11,17 @@ export const importTrainerData = (trainer: StudioTrainer, dataToImport: StudioTr
     party: cloneData.party,
     additionalDialogs: cloneData.additionalDialogs,
     resources: cloneData.resources,
+  };
+};
+
+export const importMoveData = (move: StudioMove, dataToImport: StudioMove): StudioMove => {
+  const cloneData = cloneEntity(dataToImport);
+
+  return {
+    ...cloneData,
+    id: move.id,
+    dbSymbol: move.dbSymbol,
+    type: move.type,
+    category: move.category,
   };
 };
