a32a98e522f6244679fffce411523022e6800b0c
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-11 21:49:12 +0100

    New Pokemon can be created by importing data from another Pokemon (#461)

--- src/utils/importEntityDataUtils.ts
+++ src/utils/importEntityDataUtils.ts
@@ -1,5 +1,8 @@
 import { StudioTrainer } from '@modelEntities/trainer';
+import { StudioCreature } from '@modelEntities/creature';
 import { cloneEntity } from './cloneEntity';
+import { findFirstAvailableFormTextId } from './ModelUtils';
+import { ProjectData } from '@src/GlobalStateProvider';
 
 export const importTrainerData = (trainer: StudioTrainer, dataToImport: StudioTrainer): StudioTrainer => {
   const cloneData = cloneEntity(dataToImport);
@@ -11,4 +14,32 @@ export const importTrainerData = (trainer: StudioTrainer, dataToImport: StudioTr
     additionalDialogs: cloneData.additionalDialogs,
     resources: cloneData.resources,
   };
+};
+
+export const importCreatureData = (creature: StudioCreature, dataToImport: StudioCreature, allPokemon: ProjectData['pokemon']): StudioCreature => {
+  const cloneData = cloneEntity(dataToImport);
+
+  const newCreature = {
+    ...creature,
+    forms: cloneData.forms,
+  };
+
+  newCreature.forms[0].type1 = creature.forms[0].type1;
+  newCreature.forms[0].type2 = creature.forms[0].type2;
+  newCreature.forms[0].formTextId.name = findFirstAvailableFormTextId(allPokemon, 0, 'name');
+
+  const newAllPokemon = {
+    ...allPokemon,
+    [newCreature.dbSymbol]: newCreature,
+  }; //To avoid getting same text IDs for other forms
+
+  //Update form text IDs
+  newCreature.forms.slice(1).forEach((form) => {
+    const formTextIdName = findFirstAvailableFormTextId(newAllPokemon, 0, 'name');
+    const formTextIdDescription = findFirstAvailableFormTextId(newAllPokemon, 0, 'description');
+    form.formTextId.name = formTextIdName;
+    form.formTextId.description = formTextIdDescription;
+  });
+
+  return newCreature;
 };
