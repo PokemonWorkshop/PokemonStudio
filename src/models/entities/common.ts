f65bd56bdab86c47122d54e64876164ca1811a93
Palbolsky <palbolsky@gmail.com>
2025-06-17 15:50:12 +0200

    Implement the model

--- src/models/entities/common.ts
+++ src/models/entities/common.ts
@@ -4,3 +4,8 @@ export const POSITIVE_OR_ZERO_INT = z.number().int().nonnegative();
 export const POSITIVE_INT = z.number().int().positive();
 export const POSITIVE_OR_ZERO_FLOAT = z.number().nonnegative();
 export const POSITIVE_FLOAT = z.number().positive();
+export const AUDIO_VALIDATOR = z.object({
+  name: z.string(),
+  volume: POSITIVE_OR_ZERO_INT.max(100),
+  pitch: POSITIVE_INT.min(50).max(150),
+});
