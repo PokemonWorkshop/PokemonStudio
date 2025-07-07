a47fece60f3b11fc9b2f18f4fb399b88083a8735
Palbolsky <palbolsky@gmail.com>
2025-07-07 17:06:31 +0200

    Revert "Change import by require"

--- forge.config.ts
+++ forge.config.ts
@@ -3,11 +3,8 @@ import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerNSIS } from './src/MakerNSIS';
+import { MakerAppImage } from '@reforged/maker-appimage';
 import { VitePlugin } from '@electron-forge/plugin-vite';
-
-// We use 'require' instead of 'import' to prevent a bug in the Github CI
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { MakerAppImage } = require('@reforged/maker-appimage');
 
 const config: ForgeConfig = {
   packagerConfig: {
