dfd11bf3bffcae4ee88e56eecfe703de95c69791
Palbolsky <palbolsky@gmail.com>
2025-09-28 18:38:44 +0200

    Fix style

--- src/views/components/world/event/EventTree.tsx
+++ src/views/components/world/event/EventTree.tsx
@@ -3,6 +3,19 @@ import { ClearInput } from '@components/inputs';
 import { useTranslation } from 'react-i18next';
 import { emitScrollContextMenu } from '@hooks/useContextMenu';
 import { MapTreeContainer } from '../map/tree/style';
+import styled from 'styled-components';
+
+const EventTreeContainer = styled(MapTreeContainer)`
+  .no-event {
+    ${({ theme }) => theme.fonts.normalRegular}
+    color: ${({ theme }) => theme.colors.text400};
+    padding: 9.5px 15px;
+  }
+
+  .tree-scrollbar {
+    display: none; // Remove this when implement the event tree
+  }
+`;
 
 export const EventTree = () => {
   const [research, setResearch] = useState('');
@@ -10,7 +23,7 @@ export const EventTree = () => {
   const treeScrollbarRef = useRef<HTMLDivElement>(null);
 
   return (
-    <MapTreeContainer hideMapTree={research !== ''}>
+    <EventTreeContainer hideMapTree={research !== ''}>
       <ClearInput
         value={research}
         onChange={(event) => setResearch(event.target.value)}
@@ -22,7 +35,7 @@ export const EventTree = () => {
         <div className="tree"></div>
       </div>
       {/* TODO: integrate no event in the future tree like map tree */}
-      <p>{t('no_event_found')}</p>
-    </MapTreeContainer>
+      <span className="no-event">{t('no_event_found')}</span>
+    </EventTreeContainer>
   );
 };
