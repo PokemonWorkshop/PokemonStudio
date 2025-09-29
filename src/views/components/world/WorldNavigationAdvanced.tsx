6010a7555cc12cff068be9a115588468751e8c6b
AntoinePoree <poree47@gmail.com>
2025-09-29 17:45:47 +0200

    fix: smth weird about redirection

--- src/views/components/world/WorldNavigationAdvanced.tsx
+++ src/views/components/world/WorldNavigationAdvanced.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
 import { NavigationDatabaseStyle } from '@components/database/navigation/NavigationDatabase/NavigationDatabaseStyle';
 import { NavigationDatabaseItem } from '@components/database/navigation/NavigationDatabaseItem';
-import { findMenuByPath, getDefaultMenu } from './menuConfig';
+import { MapMenu } from './map';
+import { EventMenu } from './event/EventMenu';
 
 const WorldNavigationStyle = styled.div`
   display: flex;
@@ -27,48 +27,44 @@ const WorldBuildingNavigationStyle = styled(NavigationDatabaseStyle)`
   gap: 16px;
 `;
 
-// Hook personnalisé pour gérer la navigation conditionnelle
-const useConditionalMenu = () => {
-  const location = useLocation();
-
-  const getCurrentMenu = () => {
-    const menuConfig = findMenuByPath(location.pathname);
-    return menuConfig || getDefaultMenu();
-  };
-
-  return {
-    currentMenu: getCurrentMenu(),
-    pathname: location.pathname
-  };
+const routeLinks = {
+  events: '/world/events',
+  map: '/world/map',
+  maplinks: '/world/maplinks',
 };
 
 export const WorldNavigationAdvanced = () => {
   const { t } = useTranslation();
-  const { currentMenu } = useConditionalMenu();
 
-  // Récupération de tous les menus pour la navigation
   const navigationItems = [
-    { path: '/world/map', label: t('maps') },
-    { path: '/world/events', label: t('events') },
-    { path: '/world/maplinks', label: t('maplinks') }
+    { path: routeLinks.map, label: t('maps') },
+    { path: routeLinks.events, label: t('events') },
+    { path: routeLinks.maplinks, label: t('maplinks') },
   ];
+
+  const getMenuComponent = (pathname: string) => {
+    switch (pathname) {
+      case routeLinks.map:
+      case routeLinks.maplinks:
+        return <MapMenu />;
+      case routeLinks.events:
+        return <EventMenu />;
+      default:
+        return <MapMenu />;
+    }
+  };
 
   return (
     <WorldNavigationStyle>
       <WorldBuildingNavigationStyle>
         <WorlMapsEventDiv>
           {navigationItems.map((item) => (
-            <NavigationDatabaseItem
-              key={item.path}
-              path={item.path}
-              label={item.label}
-            />
+            <NavigationDatabaseItem key={item.path} path={item.path} label={item.label} />
           ))}
         </WorlMapsEventDiv>
       </WorldBuildingNavigationStyle>
 
-      {/* Rendu conditionnel du menu basé sur le path */}
-      <currentMenu.component />
+      {getMenuComponent(location.pathname)}
     </WorldNavigationStyle>
   );
 };
