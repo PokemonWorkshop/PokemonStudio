754c17d96161172e5fcf59b8287841979dbad2a6
Palbolsky <palbolsky@gmail.com>
2025-10-07 20:41:19 +0200

    Fix wrong import

--- src/views/components/event/EventCommands.tsx
+++ src/views/components/event/EventCommands.tsx
@@ -1,6 +1,6 @@
 import BackIcon from '@assets/icons/global/back.svg';
 import { EventCommand } from './EventCommand';
-import type { StudioEventCommandCategory } from './EventCommandCategories';
+import type { StudioEventCommandCategory } from '@modelEntities/event';
 import { useTranslation } from 'react-i18next';
 import { TFunction } from 'i18next';
 import styled from 'styled-components';
