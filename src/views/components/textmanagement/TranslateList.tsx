94f0a6d9193481793b29f686b6e668ac47d3e6a1
GaburaisuVGC <matthieu.barbe01@gmail.com>
2025-10-19 14:50:04 +0200

    Added a text index reset when switching text files in the translation page (#659)

--- src/views/components/textmanagement/TranslateList.tsx
+++ src/views/components/textmanagement/TranslateList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTranslationPage } from '@hooks/usePage';
 import { DataGrid } from '@components/database/dataBlocks';
