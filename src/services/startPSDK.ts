a8aea04d6f67588a10522f73bddd005fb7e4cf61
Menta <36339268+AntoinePoree@users.noreply.github.com>
2025-10-25 13:49:24 +0200

    Users are now able to launch PSDK games from Studio even if there is some special characters in the path (#668)

--- src/services/startPSDK.ts
+++ src/services/startPSDK.ts
@@ -7,19 +7,20 @@ import log from 'electron-log';
 
 export const getSpawnArgs = (projectPath: string, ...args: string[]): [string, string[]] => {
   if (process.platform === 'win32') {
-    return ['cmd.exe', ['/c', `"${projectPath}/psdk.bat" ${args.join(' ')}`]];
+    return ['cmd.exe', ['/c', 'psdk.bat', ...args]];
   } else if (process.platform === 'linux') {
-    const linuxArgs = ['-e', `"${projectPath.replaceAll(' ', '\\ ')}/game-linux.sh ${args.join(' ')}"`];
     if (existsSync('/usr/bin/gnome-terminal')) {
-      return ['gnome-terminal', linuxArgs];
+      return ['gnome-terminal', ['--', './game-linux.sh', ...args]];
     }
     if (existsSync('/usr/bin/konsole')) {
-      return ['konsole', linuxArgs];
+      return ['konsole', ['-e', './game-linux.sh', ...args]];
     }
-    log.info('No terminal found. The PSDK console will not be displayed.');
+    if (existsSync('/usr/bin/xterm')) {
+      return ['xterm', ['-e', `bash -c "./game-linux.sh ${args.join(' ')}; exec bash"`]];
+    }
+    log.warn('No terminal found. The PSDK console will not be displayed.');
     return ['./game-linux.sh', args];
   } else if (process.platform === 'darwin') {
-    // TODO: figure out how to display the console :v
     return ['./game-mac.sh', args];
   } else {
     return ['./game.rb', args];
@@ -31,6 +32,7 @@ const bootLoadFilename = () => {
   if (process.platform === 'linux') return 'game-linux.sh';
   return 'psdk.bat';
 };
+
 const generateBootLoadContent = (projectPath: string) => {
   if (process.platform === 'darwin') return generateGameMacFileContent(projectPath);
   if (process.platform === 'linux') return generateGameLinuxFileContent(projectPath);
@@ -66,16 +68,48 @@ export const startPSDK = (projectPath: string) => {
   if (!canLaunchPSDK()) return;
 
   RMXP2StudioSafetyNet(projectPath);
-  const childProcess = spawn(...getSpawnArgs(projectPath), { cwd: projectPath, shell: true, detached: true, stdio: 'ignore' });
-  childProcess.unref();
+
+  const studioPath = process.cwd();
+  try {
+    process.chdir(projectPath);
+
+    const [command, spawnArgs] = getSpawnArgs(projectPath);
+
+    const childProcess = spawn(command, spawnArgs, {
+      cwd: projectPath,
+      shell: true,
+      detached: true,
+      stdio: 'ignore',
+    });
+
+    childProcess.unref();
+  } finally {
+    process.chdir(studioPath);
+  }
 };
 
 const startPSDKWithArgs = (projectPath: string, ...args: string[]) => {
   if (!canLaunchPSDK()) return;
 
   RMXP2StudioSafetyNet(projectPath);
-  const childProcess = spawn(...getSpawnArgs(projectPath, ...args), { cwd: projectPath, shell: true, detached: true, stdio: 'ignore' });
-  childProcess.unref();
+
+  const studioPath = process.cwd();
+  try {
+    process.chdir(projectPath);
+
+    const [command, spawnArgs] = getSpawnArgs(projectPath, ...args);
+
+    const childProcess = spawn(command, spawnArgs, {
+      cwd: projectPath,
+      shell: true,
+      detached: true,
+      stdio: 'ignore',
+    });
+
+    childProcess.unref();
+  } finally {
+    process.chdir(studioPath);
+  }
 };
 
 export const startPSDKDebug = (projectPath: string) => startPSDKWithArgs(projectPath, 'debug');
