102c001fc64fab1132b9b5953c5bb0578e506039
Palbolsky <palbolsky@gmail.com>
2025-11-06 22:16:01 +0100

    Improve the style of ItemCategoryText component

--- src/views/components/database/item/editors/ItemCategoryText.tsx
+++ src/views/components/database/item/editors/ItemCategoryText.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
 import { ItemCategoryTextProps, ITEM_CATEGORY_DESCRIPTIONS } from './ItemCategoryTextProps';
-
-const CategoryDescription = styled.p`
-  margin: 8px 0 0 0;
-  color: ${({ theme }) => theme.colors.text400};
-  font-size: 14px;
-`;
 
 export const ItemCategoryText: React.FC<ItemCategoryTextProps> = ({ itemCategory }) => {
   const { t } = useTranslation();
 
   if (!itemCategory) return null;
 
-  return (
-    <CategoryDescription>
-      {t(ITEM_CATEGORY_DESCRIPTIONS[itemCategory])}
-    </CategoryDescription>
-  );
+  return <span className="helper">{t(ITEM_CATEGORY_DESCRIPTIONS[itemCategory])}</span>;
 };
