835f3c6811769ec4e4dd132bfcd36d1d0802ca9c
Green00101 <2585640230@qq.com>
2026-05-30 18:24:42 +1000

    Add configurable trainer party max size

--- src/migrations/addBaseStatMaxValueToSettings.ts
+++ src/migrations/addBaseStatMaxValueToSettings.ts
@@ -5,17 +5,24 @@ import fsPromise from 'fs/promises';
 import path from 'path';
 import { deletePSDKDatFile } from './migrateUtils';
 
-const PRE_MIGRATION_SETTINGS_CONFIG_VALIDATOR = SETTINGS_CONFIG_VALIDATOR.omit({ baseStatMaxValue: true });
+const PRE_MIGRATION_SETTINGS_CONFIG_VALIDATOR = SETTINGS_CONFIG_VALIDATOR.omit({
+  baseStatMaxValue: true,
+  trainerPartyMaxSize: true,
+});
 
 export const addBaseStatMaxValueToSettings = async (_: IpcMainEvent, projectPath: string) => {
   deletePSDKDatFile(projectPath);
+
   const settingsFilePath = path.join(projectPath, 'Data/configs/settings_config.json');
+
   const settingsFile = await fsPromise.readFile(settingsFilePath, { encoding: 'utf-8' });
   const settingsFileParsed = PRE_MIGRATION_SETTINGS_CONFIG_VALIDATOR.safeParse(parseJSON(settingsFile, 'settings_config.json'));
   if (!settingsFileParsed.success) throw new Error('Fail to parse settings_config.json file');
-  const newSettingsFile: StudioSettingConfig = {
+
+  const newSettingsFile: Omit<StudioSettingConfig, 'trainerPartyMaxSize'> = {
     ...settingsFileParsed.data,
     baseStatMaxValue: 999,
   };
+
   await fsPromise.writeFile(settingsFilePath, JSON.stringify(newSettingsFile, null, 2));
 };
