82575fdd0dff09bd05974efca5e7f0aa8a3f0798
Yijun Gao <2585640230@qq.com>
2026-06-06 19:47:55 +1000

    Add configurable trainer party size (#759)

---
+++ src/migrations/addTrainerPartyMaxSizeToSettings.ts
@@ -0,0 +1,25 @@
+import { SETTINGS_CONFIG_VALIDATOR, StudioSettingConfig } from '@modelEntities/config';
+import { parseJSON } from '@utils/json/parse';
+import { IpcMainEvent } from 'electron';
+import fsPromise from 'fs/promises';
+import path from 'path';
+import { deletePSDKDatFile } from './migrateUtils';
+
+const PRE_MIGRATION_SETTINGS_CONFIG_VALIDATOR = SETTINGS_CONFIG_VALIDATOR.omit({ trainerPartyMaxSize: true });
+
+export const addTrainerPartyMaxSizeToSettings = async (_: IpcMainEvent, projectPath: string) => {
+    deletePSDKDatFile(projectPath);
+
+    const settingsFilePath = path.join(projectPath, 'Data/configs/settings_config.json');
+
+    const settingsFile = await fsPromise.readFile(settingsFilePath, { encoding: 'utf-8' });
+    const settingsFileParsed = PRE_MIGRATION_SETTINGS_CONFIG_VALIDATOR.safeParse(parseJSON(settingsFile, 'settings_config.json'));
+    if (!settingsFileParsed.success) throw new Error('Fail to parse settings_config.json file');
+
+    const newSettingsFile: StudioSettingConfig = {
+        ...settingsFileParsed.data,
+        trainerPartyMaxSize: 6,
+    };
+
+    await fsPromise.writeFile(settingsFilePath, JSON.stringify(newSettingsFile, null, 2));
+};
