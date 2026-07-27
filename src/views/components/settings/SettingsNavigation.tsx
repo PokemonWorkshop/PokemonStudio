eb181ac35e2c38783ffc8160921c9d0cabd78091
Manny <vegamanny115@gmail.com>
2026-07-27 17:40:35 -0400

    Added Shortcut Page to Settings (#787)

--- src/views/components/settings/SettingsNavigation.tsx
+++ src/views/components/settings/SettingsNavigation.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { NavigationDatabaseStyle } from '@components/database/navigation/NavigationDatabase/NavigationDatabaseStyle';
 import { NavigationDatabaseGroup } from '@components/database/navigation/NavigationDatabaseGroup';
 import { NavigationDatabaseItem } from '@components/database/navigation/NavigationDatabaseItem';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const SettingsNavigation = () => {
@@ -10,6 +10,9 @@ export const SettingsNavigation = () => {
     <NavigationDatabaseStyle>
       <NavigationDatabaseGroup title={t('user_settings')}>
         <NavigationDatabaseItem path="/settings/language" label={t('language')} />
+      </NavigationDatabaseGroup>
+      <NavigationDatabaseGroup title={t('app_settings')}>
+        <NavigationDatabaseItem path="/settings/shortcuts" label={t('keyboard_shortcuts')} />
       </NavigationDatabaseGroup>
       <NavigationDatabaseGroup title={t('integrations')}>
         <NavigationDatabaseItem path="/settings/maps" label={t('map_management')} />
