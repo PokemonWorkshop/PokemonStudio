0039431548887893af5c200d002d8b79c68bb768
Palbolsky <palbolsky@gmail.com>
2026-09-03 16:43:20 +0200

    commit work

--- src/hooks/useEventConvert.ts/useEventConvertProcessor.ts
+++ src/hooks/useEventConvert.ts/useEventConvertProcessor.ts
@@ -1,11 +1,10 @@
 import { useEventTree } from '@components/world/event/hooks/useEventTree';
 import { toAsyncProcess } from '@hooks/Helper';
-import { PROCESS_DONE_STATE, SpecialStateProcessors } from '@hooks/useProcess';
+import { DEFAULT_PROCESS_STATE, PROCESS_DONE_STATE, SpecialStateProcessors } from '@hooks/useProcess';
 import { useProjectEvents } from '@hooks/useProjectData';
 import { EVENT_NAME_TEXT_ID } from '@modelEntities/event/event';
-import { DEFAULT_EVENT_TREE } from '@modelEntities/event/event-tree';
-import { useGlobalState } from '@src/GlobalStateProvider';
-import { cloneEntity } from '@utils/cloneEntity';
+import { DEFAULT_EVENT_TREE, StudioEventTree } from '@modelEntities/event/event-tree';
+import { ProjectData, useGlobalState } from '@src/GlobalStateProvider';
 import { createEvent } from '@utils/entityCreation';
 import { addNewEventToEventTree } from '@utils/events/EventTreeUtils';
 import { useLoaderRef } from '@utils/loaderContext';
@@ -17,7 +16,6 @@ import { EventConvertFunctionBinding, EventConvertStateObject } from './types';
 const DEFAULT_BINDING: EventConvertFunctionBinding = {
   onFailure: () => {},
   onSuccess: () => {},
-  onGenericFailure: () => {},
 };
 
 export const useEventConvertProcessor = () => {
@@ -28,6 +26,8 @@ export const useEventConvertProcessor = () => {
   const setNewProjectText = useNewProjectText();
   const loaderRef = useLoaderRef();
   const binding = useRef<EventConvertFunctionBinding>(DEFAULT_BINDING);
+  const localEvents = useRef<ProjectData['events']>({ ...events });
+  const localEventTree = useRef<StudioEventTree>({ ...eventTree });
 
   const processors: SpecialStateProcessors<EventConvertStateObject> = useMemo(
     () => ({
@@ -35,52 +35,66 @@ export const useEventConvertProcessor = () => {
       read: ({ mapId, eventIds }, setState) => {
         return window.api.readRMXPEvents(
           { projectPath: state.projectPath || '', mapId, eventIds },
-          ({ rmxpEvents }) => setState({ state: 'createEvents', rmxpEvents }),
+          ({ rmxpEvents }) => setState({ state: 'createEvents', rmxpEvents, rmxpEventIdsToDbSymbols: {}, eventIndex: 0 }),
           handleFailure(setState, binding),
         );
       },
-      createEvents: ({ rmxpEvents }, setState) => {
+      createEvents: ({ rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex }, setState) => {
         return toAsyncProcess(() => {
-          if (rmxpEvents.length === 0) return setState({ state: 'done' });
-
-          const newEvent = createEvent(events);
-          const currentEventTree = eventTree ?? DEFAULT_EVENT_TREE;
+          if (rmxpEvents.length === eventIndex) {
+            return setState({ state: 'createTriggers', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex: 0, pageIndex: 0 });
+          }
+          const rmxpEvent = rmxpEvents[eventIndex];
+          const newEvent = createEvent(localEvents.current);
+          const currentEventTree = localEventTree.current ?? DEFAULT_EVENT_TREE;
           const dbSymbol = newEvent.dbSymbol;
+          localEvents.current = { ...localEvents.current, [dbSymbol]: newEvent };
+          localEventTree.current = addNewEventToEventTree(currentEventTree, dbSymbol, newEvent.id);
+          rmxpEventIdsToDbSymbols[rmxpEvent.id] = newEvent.dbSymbol;
           setEventTree(addNewEventToEventTree(currentEventTree, dbSymbol, newEvent.id));
-          setText(EVENT_NAME_TEXT_ID, newEvent.id, rmxpEvents[0].name);
+          setText(EVENT_NAME_TEXT_ID, newEvent.id, rmxpEvent.name);
           setNewProjectText(newEvent.csvFileId);
-          setEvent({ [dbSymbol]: { ...newEvent, klass: 'Event' } }, { event: dbSymbol });
-          const rmxpEvent = cloneEntity(rmxpEvents[0]);
-          delete rmxpEvents[0];
-          return setState({ state: 'createTriggers', rmxpEvents, rmxpEvent, eventDbSymbol: dbSymbol, pageIndex: 0 });
+          setEvent({ [dbSymbol]: { ...newEvent, klass: 'Event' } });
+          return setState({ state: 'createEvents', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex: ++eventIndex });
         });
       },
-      createTriggers: ({ rmxpEvents, rmxpEvent, eventDbSymbol, pageIndex }, setState) => {
+      createTriggers: ({ rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex, pageIndex }, setState) => {
         return toAsyncProcess(() => {
-          const page = rmxpEvent.pages[pageIndex];
-          if (pageIndex === rmxpEvent.pages.length) return setState({ state: 'createEvents', rmxpEvents });
-
-          const event = events[eventDbSymbol];
-          // TODO: create trigger command
-          return setState({ state: 'createCommands', rmxpEvents, rmxpEvent, eventDbSymbol, pageIndex, commandIndex: 0 });
-        });
-      },
-      createCommands: ({ rmxpEvents, rmxpEvent, eventDbSymbol, pageIndex, commandIndex }, setState) => {
-        return toAsyncProcess(() => {
-          const page = rmxpEvent.pages[pageIndex];
-          if (page.list.length === commandIndex) {
-            return setState({ state: 'createTriggers', rmxpEvents, rmxpEvent, eventDbSymbol, pageIndex: ++pageIndex });
+          if (rmxpEvents.length === eventIndex) {
+            localEvents.current = { ...events };
+            localEventTree.current = { ...eventTree };
+            binding.current.onSuccess({});
+            return setState(DEFAULT_PROCESS_STATE);
           }
 
-          const event = events[eventDbSymbol];
+          const rmxpEvent = rmxpEvents[eventIndex];
+          if (rmxpEvent.pages.length === pageIndex) {
+            return setState({ state: 'createTriggers', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex: ++eventIndex, pageIndex: 0 });
+          }
+
+          const page = rmxpEvent.pages[pageIndex];
+          const event = events[rmxpEventIdsToDbSymbols[rmxpEvent.id]];
+          // TODO: create trigger command
+          return setState({ state: 'createCommands', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex, pageIndex, commandIndex: 0 });
+        });
+      },
+      createCommands: ({ rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex, pageIndex, commandIndex }, setState) => {
+        return toAsyncProcess(() => {
+          const rmxpEvent = rmxpEvents[eventIndex];
+          const page = rmxpEvent.pages[pageIndex];
+          if (page.list.length === commandIndex) {
+            return setState({ state: 'createTriggers', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex, pageIndex: ++pageIndex });
+          }
+
+          const event = events[rmxpEventIdsToDbSymbols[rmxpEvent.id]];
           const rmxpCommand = page.list[commandIndex];
           // TODO: convert rmxp command
-          return setState({ state: 'createCommands', rmxpEvents, rmxpEvent, eventDbSymbol, pageIndex, commandIndex: ++commandIndex });
+          return setState({ state: 'createCommands', rmxpEvents, rmxpEventIdsToDbSymbols, eventIndex, pageIndex, commandIndex: ++commandIndex });
         });
       },
     }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
+    [events, eventTree],
   );
 
   return { processors, binding };
